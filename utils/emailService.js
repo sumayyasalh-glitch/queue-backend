f06d26f89381f1b0e99ca5a6116efc08cfb0fcaf
sumayya <sumayyasalh@gmail.com>
2026-09-01 11:47:31 +0530

    finish

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,11 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  family:4,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
